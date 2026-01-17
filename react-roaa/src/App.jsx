e20fd3abbe250ebfbda90f5cfc913d30493bb4c8
roaaalshdaifat <roaaalshdaifat@gmail.com>
2026-01-17 11:39:40 +0300

    first commit

---
+++ react-roaa/src/App.jsx
@@ -0,0 +1,12 @@
+import CardList from "./CardList";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <h1>Mini React Cards App</h1>
+      <CardList />
+    </div>
+  );
+}
+
+export default App;
